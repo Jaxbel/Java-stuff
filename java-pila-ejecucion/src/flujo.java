1b5e4b0f7c3306c01feca746302e0546e98b5d73
Jaxbel <jazziel.bello193@tectijuana.edu.mx>
2023-09-19 23:14:06 -0700

    Codigo de curso

---
+++ java-pila-ejecucion/src/flujo.java
@@ -0,0 +1,19 @@
+public class flujo {
+    public static void main(String[] args) {
+        System.out.println("Inicio  main");
+        metodo1();
+        System.out.println("Fin de main");
+    }
+    public static void metodo1(){
+        System.out.println("Inicio de metodo1");
+        metodo2();
+        System.out.println("Fin de metodo1");
+    }
+    public static void metodo2(){
+        System.out.println("Inicio de metodo2");
+        for(int i =1; i<= 5; i++){
+            System.out.println(i);
+        }
+        System.out.println("Fin de metodo2");
+    }
+}
